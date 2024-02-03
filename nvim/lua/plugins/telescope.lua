cc21d2248882f8b0050e591264c61b438b4adbbd
gtakacse <edit.takacs@gmail.com>
2024-02-03 15:37:12 +0100

    add generic vim keymaps

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,7 +8,6 @@ return {
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>x", ":bd<CR>", {})
     end,
   },
 }
--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,5 +2,11 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set timeoutlen=1000")
+vim.cmd("set ttimeoutlen=0")
 vim.g.mapleader = " "
+-- Generic key bindings
+vim.keymap.set("i", "jk", "<esc>", {})
+vim.keymap.set("n", "<esc><esc>", ":noh<CR><esc>", {})
+vim.keymap.set("n", "<leader>x", ":bd<CR>", {})
