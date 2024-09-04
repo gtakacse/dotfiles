fc9434ae5d995bf2d8a32a814aed2de3f0c6a97d
Edit Takacs <edit.takacs@gmail.com>
2024-09-04 07:05:41 +0200

    nvim config updates

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -28,3 +28,25 @@ vim.keymap.set("n", "<C-Left>", "<C-W><", {})
 vim.keymap.set("n", "<C-Right>", "<C-W>>", {})
 vim.keymap.set("n", "<C-Up>", "<C-W>+", {})
 vim.keymap.set("n", "<C-Down>", "<C-W>-", {})
+
+local function augroup(name)
+    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+    group = augroup("highlight_yank"),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+
+-- wrap and check for spell in text filetypes
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("wrap_spell"),
+    pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.wrap = true
+        vim.opt_local.spell = true
+    end,
+})
