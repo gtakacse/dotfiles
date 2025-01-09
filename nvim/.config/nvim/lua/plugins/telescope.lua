87ffb24f0fd19a72655654dff296fc40833ac8f4
EditTakacs <edit.takacs@gmail.com>
2025-01-09 22:44:45 +0100

    floating terminal

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -42,6 +42,7 @@ return {
             local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
             local builtin = require("telescope.builtin")
             vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
             vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
             vim.keymap.set("n", "<leader>fg", function()
                 local opts = {
