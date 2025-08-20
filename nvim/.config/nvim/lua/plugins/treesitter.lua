59935544be21bed49fa2fedc39fc3ef802ab889f
EditTakacs <edit.takacs@gmail.com>
2025-08-20 14:13:35 +0200

    treesitter updates
    disable treesitter for lua, vim, vimdoc as NeoVim 0.11.3 has issues with
    line deletions

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,26 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
     build = ":TSUpdate",
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            -- auto_install = true,
-            -- ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "python", "rust", "scala", "java" },
+            auto_install = true,
+            ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "python", "rust", "scala", "sql", "java" },
             highlight = {
                 enable = true,
-                disable = function(_, buf)
-                    local max_filesize = 100 * 1024 -- 100 KB
-                    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-                    if ok and stats and stats.size > max_filesize then
-                        return true
-                    end
-                end,
-                additional_vim_regex_highlighting = false,
+                -- disable = function(_, buf)
+                --     local max_filesize = 100 * 1024 -- 100 KB
+                --     local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+                --     if ok and stats and stats.size > max_filesize then
+                --         return true
+                --     end
+                -- end,
+                disable = { 'vim', 'vimdoc', 'lua' },
+                additional_vim_regex_highlighting = { 'ruby' },
             },
-            -- indent = { enable = true },
+            indent = { enable = true, disable = { 'ruby' } },
         })
     end,
 }
