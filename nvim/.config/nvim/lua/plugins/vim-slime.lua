ccf76e0f9b8ca790f989ace5ca57ab36ba313cc9
EditTakacs <edit.takacs@gmail.com>
2025-08-27 21:49:47 +0200

    vim-slime

---
+++ nvim/.config/nvim/lua/plugins/vim-slime.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "jpalardy/vim-slime",
+        keys = {
+            { "<leader>sc", "<cmd>SlimeConfig<cr>",               desc = "Slime Config" },
+            { "<leader>sr", "<Plug>SlimeSendCell<BAR>/^# %%<CR>", desc = "Slime Send Cell" },
+            { "<leader>sr", ":<C-u>'<,'>SlimeSend<CR>",           mode = "v",              desc = "Slime Send Selection" },
+        },
+        config = function()
+            vim.g.slime_target = "tmux"
+            vim.g.slime_cell_delimiter = "# %%"
+            vim.g.slime_bracketed_paste = 1
+        end
+    }
+}
